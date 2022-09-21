95208d3b133abacfe4884bc75d2670df955557ac
bao nam <namb1910413student.ctu.edu.vn>
2022-09-21 09:11:04 +0700

    dinh nghia cac route

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,21 @@
+exports.create=(req,res)=>{
+    res.send({massage: "create handler"});
+};
+exports.findAll=(req,res)=>{
+    res.send({massage: "findAll handler"});
+};
+exports.findOne=(req,res)=>{
+    res.send({massage: "findOne handler"});
+};
+exports.update=(req,res)=>{
+    res.send({massage: "update handler"});
+};
+exports.detele=(req,res)=>{
+    res.send({massage: "delete handler"});
+};
+exports.deteleAll=(req,res)=>{
+    res.send({massage: "deteleAll handler"});
+};
+exports.findAllFavorite=(req,res)=>{
+    res.send({massage: "findAllFavorite handler"});
+};
