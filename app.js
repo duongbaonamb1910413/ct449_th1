a33c15a543fcd6423dda0982c019d24b504e73de
bao nam <namb1910413student.ctu.edu.vn>
2022-09-21 09:24:05 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express =require("express");
 const cors =require("cors");
+const ApiError = require("./app/api-error");
 const contactRouter =require("./app/routes/contact.route");
 const app=express();
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+    });
+});
 app.use("/api/contacts",contactRouter);
 app.get("/",(req,res)=>{
     res.json({massage: "welcome to contact book application"});
